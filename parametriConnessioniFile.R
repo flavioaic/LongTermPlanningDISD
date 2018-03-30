981e60aacf6d4e167e1c44d8b99daacb8739e18c
Flavio Aicardi <flavioaicardi@hotmail.com>
2018-03-30 12:29:20 +0200

    Update parametriConnessioniFile.R
    Aggiornato con la nuova versione di voli_feb_2018.csv

--- parametriConnessioniFile.R
+++ parametriConnessioniFile.R
@@ -1,4 +1,4 @@
 param.connessione.db <- "Driver={SQL Server};server=DESKTOP-HH30FVF;database=AMs516;trusted_connection=yes;"
 
 #File
-param.file.voliStatisticheSito.csv <- "InterpretazioneDB/voli_dec_2017.CSV"
+param.file.voliStatisticheSito.csv <- "InterpretazioneDB/voli_feb_2018.csv"
